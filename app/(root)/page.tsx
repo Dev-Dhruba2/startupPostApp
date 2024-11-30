4c7335731824b2823b945064549c4331f42eab77
Dhruba Bhattacharyya <ece2023114@rcciit.org.in>
2024-12-01 01:16:41 +0530

    card Ui completed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,56 @@
 import Image from "next/image";
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
-export default async function Home({ searchParams }: {searchParams: Promise<{query?: string}>}) {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
 
-  const query = (await searchParams).query
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Dhruba" },
+      _id: 1,
+      description: "This is a description",
+      image: "https://c8.alamy.com/comp/K36XG3/i-robot-date-2004-K36XG3.jpg",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
+
   return (
     <>
       <section className="pink_container">
-        <h1 className="heading">Pitch your startup, <br /> Connect with Entrepreneurs</h1>
+        <h1 className="heading">
+          Pitch your startup, <br /> Connect with Entrepreneurs
+        </h1>
 
-        <p className="sub-heading !max-w-3xl">Submit Idead, Vote on Pitches, and Get Noticed in virtual Competitions.</p>
+        <p className="sub-heading !max-w-3xl">
+          Submit Idead, Vote on Pitches, and Get Noticed in virtual
+          Competitions.
+        </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <div className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+
+          <ul className="mt-7 card_grid">
+            {posts?.length > 0 ? (
+              posts.map((post: StartupCardType, index: number) => (
+                <StartupCard key={post?._id} post={post} />
+              ))
+            ) : (
+              <p className="no-results"> No Startups found </p>
+            )}
+          </ul>
+        </div>
       </section>
     </>
   );
